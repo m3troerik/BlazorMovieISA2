a6f1a5b0446a21237e79f1122241547990d39bc4
Erik Mägi <erimag@taltech.ee>
2026-03-23 15:42:25 +0200

    isProperty testid movietests.cs jaoks

--- Tests/Tests.Data/MovieTests.cs
+++ Tests/Tests.Data/MovieTests.cs
@@ -4,13 +4,13 @@ using Abc.Tests.Aids;
 namespace Abc.Tests.Data;
 
 [TestClass]
-public class MovieTests
+public sealed class MovieTests : BaseTests<Movie>
 {
-    [TestClass]
-
-    public sealed class MoviesTests : BaseTests<Movie>
-    {
-
-
-    }
+    [TestMethod] public void IdTest() => isProperty<int>(nameof(Movie.Id));
+    [TestMethod] public void TitleTest() => isProperty<string>(nameof(Movie.Title));
+    [TestMethod] public void ReleaseDateTest() => isProperty<DateOnly>(nameof(Movie.ReleaseDate));
+    [TestMethod] public void GenreTest() => isProperty<string>(nameof(Movie.Genre));
+    [TestMethod] public void PriceTest() => isProperty<decimal>(nameof(Movie.Price));
+    [TestMethod] public void RatingTest() => isProperty<string>(nameof(Movie.Rating));
+   
 }
