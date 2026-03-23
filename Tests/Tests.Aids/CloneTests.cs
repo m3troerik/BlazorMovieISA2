e11c207979b46ccaa93c0c8ddadbef4bdc23e18d
Erik Mägi <erimag@taltech.ee>
2026-03-23 16:20:14 +0200

    27 build errorit fixed :)

---
+++ Tests/Tests.Aids/CloneTests.cs
@@ -0,0 +1,64 @@
+﻿using Abc.Aids;
+
+    namespace Abc.Tests.Aids;
+
+[TestClass]
+public sealed class CloneTests
+{
+    private class Address
+    {
+        public string City { get; set; } = "";
+    }
+
+    private class Person
+    {
+        public string Name { get; set; } = "";
+        public Address Address { get; set; } = new();
+    }
+
+    private static Person person = null!;
+    private Person clone = null!;
+
+    [ClassInitialize]
+    public static void ClassInitialize(TestContext context)
+    {
+        person = new Person
+        {
+            Name = "John Doe",
+            Address = new Address
+            {
+                City = "New York"
+            }
+        };
+    }
+
+    [TestInitialize] public void TestInitialize() => clone = Abc.Aids.Clone.Object(person);
+    [TestCleanup] public void TestCleanup() => clone = null!;
+
+    [TestMethod] public void CloneTest() => Assert.AreNotSame(person, clone);
+    [TestMethod] public void SamePersonNameTest() => Assert.AreEqual(person.Name, clone.Name);
+    [TestMethod] public void SameCityTest() => Assert.AreEqual(person.Address.City, clone.Address.City);
+
+    [TestMethod]
+    public void ChangePersonNameTest()
+    {
+        clone.Name = "Bla Bla";
+        Assert.AreEqual("John Doe", person.Name);
+    }
+
+    [TestMethod] public void NotSameAddressTest() => Assert.AreNotSame(person.Address, clone.Address);
+
+    [TestMethod]
+    public void ChangeCityTest()
+    {
+        clone.Address.City = "Bla Bla";
+        Assert.AreEqual("New York", person.Address.City);
+    }
+
+    [TestMethod]
+    public void ChangeAddressTest()
+    {
+        clone.Address = new Address { City = "Bla Bla" };
+        Assert.AreEqual("New York", person.Address.City);
+    }
+}
